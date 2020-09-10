896f4a1b17d6855d02c55702e06cbdb3609f8e1a
Joey Ezechiels <joey.ezechiels@gmail.com>
2020-09-10 11:00:37 +0200

    Add ES lint file

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'eslint:recommended'
+    ],
+    rules: {
+        'no-console' : process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)'
+            ],
+            env: {
+                mocha: true
+            }
+        }
+    ]
+}
